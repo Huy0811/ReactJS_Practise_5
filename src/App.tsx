ca3bb04fdc0a7343f9cbaa13080864f61c80264d
Huy0811 <duongchihuy.study@gmail.com>
2022-05-28 15:52:41 +0700

    change color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Child from "./components/Child";
+import ColorList from "./components/ColorList";
 import Header from "./components/Header";
 import Parent from "./components/Parent";
 
@@ -9,7 +10,7 @@ const ImgStyle = {
   height: "70px",
 };
 
-const CommandTexts = [
+const InitialCommandText = [
   {
     name: "Add",
     style: {
@@ -53,12 +54,28 @@ const CommandTexts = [
 ];
 
 function App() {
+  const [commandText, setCommandText] = useState(InitialCommandText);
   return (
     <div className="App">
       <Header style={ImgStyle} />
       <Parent>
-        <Child names={CommandTexts} />
+        <Child names={commandText} />
       </Parent>
+      <ColorList
+        handleChange={(event) => {
+          setCommandText(
+            commandText.map((item) => {
+              return {
+                ...item,
+                style: {
+                  ...item.style,
+                  color: event.currentTarget.value,
+                },
+              };
+            })
+          );
+        }}
+      />
     </div>
   );
 }
--- src/components/ColorList.tsx
+++ src/components/ColorList.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import "./ColorList.css";
 
-const ColorList = () => {};
+type ColorListProps = {
+  handleChange: (event: React.MouseEvent<HTMLInputElement>) => void;
+};
+
+const ColorList = (props: ColorListProps) => {
+  return (
+    <div className="container">
+      <div className="colorList1">
+        <input
+          type="radio"
+          name="selectColor"
+          value="aqua"
+          onClick={props.handleChange}
+        />
+      </div>
+      <div className="colorList2">
+        <input
+          type="radio"
+          name="selectColor"
+          value="blueviolet"
+          onClick={props.handleChange}
+        />
+      </div>
+      <div className="colorList3">
+        <input
+          type="radio"
+          name="selectColor"
+          value="palevioletred"
+          onClick={props.handleChange}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default ColorList;
